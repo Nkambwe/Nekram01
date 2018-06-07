5ad1ac06d35d6426905dacface1d068f5c66ed2f
Nkambwe <jo.jomac.mac801@gmail.com>
2018-06-07 17:09:04 +0300

    HTL Templates

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,22 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Macjohnan'}
-    return render_template("index.html", user=user)
+    posts=[
+            {
+                'author':{'username':'Steven'},
+                'body':'Beatiful day in Uganda'
+            },
+            {
+                'author':{'username':'Efrance'},
+                'body':"Mother's happy moments"
+            },
+            {
+                'author':{'username':'Donald Trump'},
+                'body':"Enviroment degradation hocks"
+            },
+            {
+                'author':{'username':'Kovosky D'},
+                'body':"World cup in Russia"
+            }
+        ]
+    return render_template("index.html", title='Home', user=user, posts=posts)
